22d47b65447e1f2a6c036074d0651e3e25b44585
Bernardo Prieto <123491333+BernardoMDPrieto@users.noreply.github.com>
2023-10-28 15:16:37 -0300

    Update script.js

--- exercicio80/js/script.js
+++ exercicio80/js/script.js
@@ -1,11 +1,13 @@
 // Crie um programa que leia o nome e a idade de várias pessoas até que o nome "fim" 
 // seja informado. Ao final, exiba a média das idades. 
 let nome; idadeTotal = 0  
-for(let i = 0;nome != "fim";i++) {
+let contadora = 0
+for(contadora = 0;nome != "fim";contadora++) {
     nome = prompt("Informar algum nome?")
     if(nome != "fim"){
     idade = parseInt(prompt("Informe a idade de " + nome ))
     idadeTotal+=idade
     }
 }
-alert(idadeTotal)
+let media = idadeTotal/contadora
+alert(media)
