c963c4ad0512905ccf28955364e368d248a8fbb3
Bernardo Prieto <123491333+BernardoMDPrieto@users.noreply.github.com>
2023-10-28 15:20:30 -0300

    Update script.js

--- exercicio02/js/script.js
+++ exercicio02/js/script.js
@@ -1,4 +1,5 @@
 // Ler o raio de um circulo e calcular o perímetro dele
 let raio = parseFloat(prompt("Informe o raio do circulo"))
 let perimetro = raio * 2 * Math.PI 
-alert(`O perimetro do circulo é de ${perimetro}`)
+let area = raio**2 * Math.PI
+alert(`O perimetro do circulo é de ${perimetro} \n e sua área é de ${area}`)
